e591388bdc3b8c05ed4aa2914f81ae0d21db7369
Daniel <danielum.re@gmail.com>
2025-02-20 12:04:26 +0100

    Formulario reactivo

--- src/app/componentes/formulario-cliente/formulario-cliente.component.ts
+++ src/app/componentes/formulario-cliente/formulario-cliente.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario-cliente',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './formulario-cliente.component.html',
   styleUrl: './formulario-cliente.component.css'
 })
 export class FormularioClienteComponent {
+  public formulario:FormGroup;  // variable que se le asigna al formulario
 
+  // variables para los campos del formulario
+  public id:FormControl;
+  public nombre:FormControl;
+  public cif:FormControl;
+  public direccion:FormControl;
+  public grupo:FormControl;
+
+
+  // metodo constructor
+  constructor() {
+    this.id = new FormControl(-1);
+    this.nombre = new FormControl("");
+    this.cif = new FormControl("");
+    this.direccion = new FormControl("");
+    this.grupo = new FormControl("");
+
+    this.formulario = new FormGroup({ id:this.id , nombre:this.nombre , cif:this.cif , direccion:this.direccion , grupo:this.grupo });
+  }
 }
