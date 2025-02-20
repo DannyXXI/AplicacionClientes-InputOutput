09e59d38c042c3f444a2f22c384ea62bd59c5a13
Daniel <danielum.re@gmail.com>
2025-02-20 14:27:48 +0100

    Obtener datos del cliente a editar

--- src/app/componentes/listado-clientes/listado-clientes.component.ts
+++ src/app/componentes/listado-clientes/listado-clientes.component.ts
@@ -11,12 +11,27 @@ import { Cliente } from '../../modelos/Cliente';
 export class ListadoClientesComponent {
   public listaClientes:Cliente[];  // array que contiene la lista de clientes
 
+  public botonesDeshabilitados:boolean; // variable para inhabiliatar los botones cuando se entre en modo edicion de un cliente
+
+  public clienteAEditar:Cliente | undefined; // variable que contendrá el cliente a editar que sera mandado al formulario
+
   constructor() {
     this.listaClientes = [];
+
+    this.botonesDeshabilitados = false;
+
+    this.clienteAEditar = undefined;
   }
 
   // metodo para recibir el cliente del formulario (hijo)
   public recibirCliente(e:Cliente):void {
     this.listaClientes.push(e);
+  }
+
+  // metodo para mandar el cliente a editar al formulario
+  public editarCliente(id:number){
+    this.clienteAEditar = this.listaClientes.find( cliente => cliente.id == id); // buscamos el cliente con el id
+    this.botonesDeshabilitados=true;
+    console.log(this.clienteAEditar);
   }
 }
